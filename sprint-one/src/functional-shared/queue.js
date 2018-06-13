9570af349ec7ec2863083e0ceb47d48849e4d81d
javier_carey <38820531+jav1jav@users.noreply.github.com>
2018-06-13 15:47:08 -0700

    resolved sprint-one through functional-shared instantiation

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,31 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var queueInstance = {}
+  queueInstance.storage = {}
+  queueInstance.storageStart = 0;
+  queueInstance.storageEnd = 0;
+  _.extend(queueInstance, queueMethods)
+
+  return queueInstance;
+
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function (value) {
+  this.storage[this.storageEnd] = value;
+  this.storageEnd++;
+}
+
+queueMethods.dequeue = function () {
+  var x = this.storage[this.storageStart];
+  delete this.storage[this.storageStart];
+  this.storageStart++;
+  return x;
+}
+
+queueMethods.size = function () {
+  return this.storageEnd - this.storageStart < 0 ? 0 : this.storageEnd - this.storageStart;
+}
 
