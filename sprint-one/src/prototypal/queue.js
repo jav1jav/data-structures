ea8c013d048e9e9ef9ddbe6ca9c677cb53307360
javier_carey <38820531+jav1jav@users.noreply.github.com>
2018-06-13 18:13:21 -0700

    completed prototypal instantiation

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,29 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj = Object.create(queueMethods);
+  obj.storage = {}
+  obj.storageStart = 0;
+  obj.storageEnd = 0;
+
+  return obj;
+
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function(value) {
+  this.storage[this.storageEnd] = value;
+  this.storageEnd++;
+}
 
+queueMethods.dequeue = function() {
+  var x = this.storage[this.storageStart];
+  delete this.storage[this.storageStart];
+  this.storageStart++;
+  return x;
+}
+
+queueMethods.size = function() {
+  return this.storageEnd - this.storageStart < 0 ? 0 : this.storageEnd - this.storageStart;
+}
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,28 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+
+  var obj = Object.create(stackMethods);
+  obj.storage = {}
+  obj.storageKey = 0;
+
+  return obj;
+
 };
 
 var stackMethods = {};
 
 
+stackMethods.push = function(value) {
+  this.storage[this.storageKey] = value;
+  this.storageKey++;
+}
+
+stackMethods.pop = function() {
+  var x = this.storage[this.storageKey - 1];
+  delete this.storage[this.storageKey - 1];
+  this.storageKey--;
+  return x;
+}
+
+stackMethods.size = function() {
+  return this.storageKey < 0 ? 0 : this.storageKey;
+}
