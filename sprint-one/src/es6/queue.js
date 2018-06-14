e5c8c11a601c6e47e4510b9be7eca3a39a59e965
javier_carey <38820531+jav1jav@users.noreply.github.com>
2018-06-13 20:16:16 -0700

    completed es6 instantiation

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,25 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.storageStart = 0;
+    this.storageEnd = 0;
+  }
+
+  enqueue(value) {
+    this.storage[this.storageEnd] = value;
+    this.storageEnd++;
+  }
+
+  dequeue() {
+    var x = this.storage[this.storageStart];
+    delete this.storage[this.storageStart];
+    this.storageStart++;
+    return x;
+  }
+
+  size() {
+    return this.storageEnd - this.storageStart < 0 ? 0 : this.storageEnd - this.storageStart;
   }
 
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,24 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.storageKey = 0;
+  }
+
+  push(value) {
+    this.storage[this.storageKey] = value;
+    this.storageKey++;
+  }
+
+  pop() {
+    var x = this.storage[this.storageKey - 1];
+    delete this.storage[this.storageKey - 1];
+    this.storageKey--;
+    return x;
+  }
+
+  size() {
+    return this.storageKey < 0 ? 0 : this.storageKey;
   }
 
 }
